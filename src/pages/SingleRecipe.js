b10b57f3fd908c72fd27f693504400fff9a033df
Yassineab27 <45517237+Yassineab27@users.noreply.github.com>
2019-05-10 14:33:58 +0000

    Modifying SinglePage

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import {recipeData} from "../data/tempDetails";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 class SingleRecipes extends Component {
     constructor(props) {
@@ -7,12 +9,54 @@ class SingleRecipes extends Component {
         this.state = {
             recipe: recipeData
         };
+        console.log(props.match.params.id)
     };
 
+    
     render() {
+        const {recipe} = this.state;
+        const recipeInfo = recipe ? (
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-6 my-3">
+                        <Link to="/recipes" className="btn btn-warning mb-4">
+                            <i className="fas fa-undo-alt"></i> back to recipes list
+                        </Link>
+                        <img src={recipe.image_url} alt={recipe.title} className="d-block w-100 Singlecard-img"/> 
+                    </div>
+                    <div className="col-10 mx-auto col-md-6 my-3">
+                        <h3 className="text-goldish">{recipe.title}</h3>
+                        <h6 className="text-dark" style={{fontStyle: "italic"}}>published by <strong>{recipe.publisher}</strong></h6>
+                        <ul className="list-group mb-4">
+                            <h2 className="mt-3 mb-4 text-goldish">Ingredients</h2>
+                            {recipe.ingredients.map((item, index) => {
+                                return(
+                                    <li key={index} className="list-group-item text-dark">
+                                        {item}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <a href={recipe.publisher_url} target="_blank" rel="noopenernoreferrer" className="btn btn-success mt-2 mx-2"><i className="fas fa-user"></i> publisher</a>
+                        <a href={recipe.source_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary mt-2 mx-2"><i className="fas fa-globe"></i> Page Url </a>
+                    </div>
+                </div>
+            </div>
+            
+        ) : (
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-6 my-3">
+                        <h2 className="text-orange text-center">
+                            loading recipe...
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        )
         return(
             <div>
-                <h1>{this.state.recipe.title}</h1>
+                {recipeInfo}
             </div>
         )
     };
